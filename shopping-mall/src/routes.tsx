327a2fc4b354aad1ebaf6191b63ed1522e07b1c2
hyunjun Yang <yhj0439@nate.com>
2022-10-28 18:40:39 +0900

    상품목록 페이지 생성 & nav바 생성(홈,상품페이지,장바구니) => route 생성

--- shopping-mall/src/routes.tsx
+++ shopping-mall/src/routes.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./pages/cart";
 import GlobalLayout from "./pages/_layout";
 
 const Index = React.lazy(() => import("./pages/index"));
@@ -13,6 +14,7 @@ export const routes = [
       { path: "/", element: <Index />, index: true },
       { path: "/products", element: <ProductsIndex />, index: true },
       { path: "/products/:id", element: <ProductsId /> },
+      { path: "/cart", element: <Cart />, index: true },
     ],
   },
 ];
@@ -21,4 +23,5 @@ export const pages = [
   { route: "/" },
   { route: "/products" },
   { route: "/products/:id" },
+  { route: "/cart" },
 ];
