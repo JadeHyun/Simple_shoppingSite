4df9569d93d96f44acdc23e5c8838661283720d0
hyunjun Yang <yhj0439@nate.com>
2022-11-03 15:45:17 +0900

    장바구니 수량 기능 완서ㅓㅇ

--- shopping-mall/src/components/cart/item.tsx
+++ shopping-mall/src/components/cart/item.tsx
@@ -1,12 +1,39 @@
 import { SyntheticEvent } from "react";
-import { useMutation } from "react-query";
+import { QueryClient, useMutation } from "react-query";
 import { CartType, UPDATE_CART } from "../../graphql/cart";
-import { graphqlFetcher } from "../../queryClient";
+import { getClient, graphqlFetcher, QueryKeys } from "../../queryClient";
 
 const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
+  const queryClient = getClient();
   const { mutate: updateCart } = useMutation(
     ({ id, amount }: { id: string; amount: number }) =>
-      graphqlFetcher(UPDATE_CART, { id, amount })
+      graphqlFetcher(UPDATE_CART, { id, amount }),
+    {
+      onMutate: async ({ id, amount }) => {
+        await queryClient.cancelQueries(QueryKeys.CART);
+        const prevCart = queryClient.getQueriesData<{
+          [key: string]: CartType;
+        }>(QueryKeys.CART);
+        if (!prevCart?.[id]) return prevCart;
+
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: { ...prevCart[id], amount },
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+        return prevCart;
+      },
+      onSuccess: (newValue) => {
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(
+          QueryKeys.CART
+        );
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: newValue,
+        };
+        queryClient.setQueriesData(QueryKeys.CART, newCart);
+      },
+    }
   );
 
   const handleUpdateAmount = (e: SyntheticEvent) => {
@@ -16,7 +43,7 @@ const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
 
   return (
     <li className="cart-item">
-      <img src={imageUrl} alt="더미데이터 이미지" />
+      <img className="cart-item_image" src={imageUrl} alt="더미데이터 이미지" />
       <p className="cart-item_price"> {price}</p>
       <p className="cart-item_title"> {title}</p>
       <input
