ca2d4b53db7cebe35949417c7974064ffe24ed8d
hyunjun Yang <yhj0439@nate.com>
2022-11-09 18:59:34 +0900

    patment graphql 작성 중

--- shopping-mall/src/components/payment/index.tsx
+++ shopping-mall/src/components/payment/index.tsx
@@ -1,15 +1,36 @@
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { checkedCartState } from "../../recoils/cart";
 import WillPay from "../willPay";
+import PaymentModal from "./modal";
 
 const Payment = () => {
-  const [checkedCartDate, setCheckedCartData] =
-    useRecoilState(checkedCartState);
+  const navigate = useNavigate();
+  const setCheckedCartData = useSetRecoilState(checkedCartState);
+  const [modalShown, toggleModal] = useState(false);
   const showModal = () => {
-    return;
+    toggleModal(true);
   };
 
-  return <WillPay submitTitle="결제하기" handleSubmit={showModal} />;
+  const proceed = () => {
+    //결제진행!
+
+    setCheckedCartData([]);
+    navigate("/products", { replace: true });
+  };
+
+  const cancel = () => {
+    toggleModal(false);
+  };
+
+  return (
+    <div>
+      <WillPay submitTitle="결제하기" handleSubmit={showModal} />
+      <PaymentModal show={modalShown} proceed={proceed} cancel={cancel} />
+    </div>
+  );
 };
 
 export default Payment;
--- shopping-mall/src/mocks/handlers.ts
+++ shopping-mall/src/mocks/handlers.ts
@@ -7,6 +7,7 @@ import {
   UPDATE_CART,
   DELETE_CART,
 } from "../graphql/cart";
+import { EXECUTE_PAY } from "../graphql/payment";
 
 const mockProducts = (() =>
   Array.from({ length: 20 }).map((_, i) => ({
@@ -75,5 +76,9 @@ export const handlers = [
     delete newData[id];
     cartData = newData;
     return res(ctx.data(id));
+  }),
+
+  graphql.mutation(EXECUTE_PAY, ({ variables }, res, ctx) => {
+    return res();
   }),
 ];
